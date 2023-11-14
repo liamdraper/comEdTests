0e4e97bd06673b948f44824317c25080b668d31c
liamdraper <liamdraper83@gmail.com>
2023-11-14 09:18:44 -0600

    Re-oranigzed files

--- dev_environment_test.py
+++ dev_environment_test.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
 
 class ConctactFormTest(unittest.TestCase):
 
@@ -25,20 +26,21 @@ class ConctactFormTest(unittest.TestCase):
             EC.presence_of_element_located((By.ID, 'tfa_FirstName'))
         )
 
+        print('test')
+
         # Now not typing keys in
         # Identifying page elements within variables
         first_name = self.driver.find_element(By.ID, 'tfa_FirstName')
         last_name = self.driver.find_element(By.ID, 'tfa_LastName')
         email = self.driver.find_element(By.ID, 'tfa_Email')
         confirm_email = self.driver.find_element(By.ID, 'tfa_ConfirmEmail')
-        next_page_button1 = self.driver.find_element(By.ID, '#wfPageNextId1')
+        next_page_button1 = self.driver.find_element(By.ID, 'wfPageNextId1')
 
         # Fill in form
         first_name.send_keys("John")
         last_name.send_keys("Doe")
         email.send_keys("johndoe@example.com")
         confirm_email.send_keys("johndoe@example.com")
-
         next_page_button1.click()
 
         WebDriverWait(self.driver, 10).until(
@@ -55,7 +57,7 @@ class ConctactFormTest(unittest.TestCase):
         service_city = self.driver.find_element(By.ID, 'tfa_7945020484415')
         service_state = self.driver.find_element(By.ID, 'tfa_7945020484416')
         service_zip = self.driver.find_element(By.ID, 'tfa_7945020484417')
-        next_page_button2 = self.driver.find_element(By.Id, '#wfPageNextId2')
+        next_page_button2 = self.driver.find_element(By.ID, 'wfPageNextId2')
 
         phone.send_keys('7732694037')
         street_address.send_keys('322 S Green St #300')
@@ -73,15 +75,49 @@ class ConctactFormTest(unittest.TestCase):
         )
 
         account_number = self.driver.find_element(By.ID, 'tfa_AccountNumbernod')
-        next_page_button3 = self.driver.find_element(By.ID, '#wfPageNextId3')
+        next_page_button3 = self.driver.find_element(By.ID, 'wfPageNextId3')
 
         account_number.send_keys('1234567890')
         next_page_button3.click()
 
         WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.ID, 'tfa_AccountNumbernod'))
+            EC.presence_of_element_located((By.ID, 'tfa_7945020484360'))
         )
+
+        option1 = self.driver.find_element(By.ID, 'tfa_7945020484522')
+        option2 = self.driver.find_element(By.ID, 'tfa_7945020484395')
+        drop_down_menu = self.driver.find_element(By.ID, 'tfa_7945020484398')
+        next_page_button4 = self.driver.find_element(By.ID, 'wfPageNextId4')
+
+        option1.click()
+        option2.click()
+        select = Select(drop_down_menu)
+        select.select_by_visible_text('Acura NSX')
+        next_page_button4.click() 
         
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.ID, 'tfa_7945020484355-HTML'))
+        )
+
+        checkbox = self.driver.find_element(By.ID, 'tfa_7945020484355-HTML')
+        alert_email = self.driver.find_element(By.ID, 'tfa_7945020484423')
+        next_page_button5 = self.driver.find_elements(By.ID, 'wfPageNextId5')
+
+        checkbox.click()
+        alert_email.send_keys('johndoe@example.com')
+        next_page_button5.click()
+
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.ID, 'tfa_referalQuestion-L'))
+        )
+
+        no_referral = self.driver.find_element(By.ID, 'tfa_referalQuestionNo')
+        community_meeting = self.driver.find_element(By.ID, 'tfa_CommunityMeeting')
+        next_page_button6 = self.driver.find_element(By.ID, 'wfPageNextId6')
+
+        no_referral.click()
+        community_meeting.click()
+        next_page_button6.click()
 
     def tearDown(self):
         self.driver.close()
